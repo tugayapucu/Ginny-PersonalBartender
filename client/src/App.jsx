6340afd1eecb4e1b240bc69458abc6917ea1f6db
tugayapucu <tugayapucu@gmail.com>
2026-03-15 02:47:54 +0300

    refactor(App): remove unused useState import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
